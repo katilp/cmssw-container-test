1b149d2cda4bb43aa88781399238db636cf69f4b
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2020-06-01 20:56:58 +0200

    Output from edmDumpEventContent

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,8 +15,8 @@ cd $2/src
 eval `scramv1 runtime -sh`
 echo "$2 is at your service."
 
- 
-edmProvDump --sort $1 > dump.txt
+# This script only dumps the event content of one open data file. Here one could as well have a short cmsRun jon analyzing data.
+edmDumpEventContent $1 > dump.txt
 sudo chown -R cmsusr /github/workspace
 chmod 755 /github/workspace
 cp dump.txt /github/workspace
